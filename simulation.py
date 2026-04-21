abcf685ea0987299ea58dd1e90ff1a1c5059094e
Desire Igweze <114906468+desire-igweze@users.noreply.github.com>
2026-04-21 10:10:45 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -2,27 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parameters
-m = 1.0
-k = 2.0
-b = 0.5
+m = 1.0      # mass
+k = 2.0      # spring constant
+b = 0.5      # damping coefficient
 
+# Time settings
 t = np.linspace(0, 20, 1000)
 dt = t[1] - t[0]
 
+# Initial conditions
 x = np.zeros(len(t))
 v = np.zeros(len(t))
 
-x[0] = 1.0
-v[0] = 0.0
+x[0] = 1.0   # initial displacement
+v[0] = 0.0   # initial velocity
 
-for i in range(len(t)-1):
+# Euler method
+for i in range(len(t) - 1):
     a = -(b/m)*v[i] - (k/m)*x[i]
-    v[i+1] = v[i] + a*dt
-    x[i+1] = x[i] + v[i]*dt
+    v[i+1] = v[i] + a * dt
+    x[i+1] = x[i] + v[i] * dt
 
+# Plot result
+plt.figure()
 plt.plot(t, x)
 plt.title("Damped Harmonic Oscillator")
 plt.xlabel("Time")
 plt.ylabel("Displacement")
 plt.grid()
+
+# Save image (for GitHub)
+plt.savefig("output.png")
+
 plt.show()
